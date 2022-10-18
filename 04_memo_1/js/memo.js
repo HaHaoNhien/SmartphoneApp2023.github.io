6a113e8a26e7a7eea21b7bccada6a146e27cc64c
HaHaoNhien <pandagau2211@gmail.com>
2022-10-18 12:32:12 +0900

    list infor

--- 04_memo_1/js/memo.js
+++ 04_memo_1/js/memo.js
@@ -8,12 +8,13 @@ window.addEventListener("DOMContentLoaded",
             window.alert("このブラウザはLocal　Storage　機能が実装されていません。");
             return;
         } else {
-            savelocalStorage(); // 2.Local Storage への保存
+            viewStorage();  // localStorage からのデータの取得とテーブルへ表示
+            saveLocalStorage(); // 2.Local Storage への保存
         }
     },false
 );
         // 2. Local Storage への保存
-function savelocalStorage(){
+function saveLocalStorage(){
     const save = document.getElementById("save");
     save.addEventListener("click",
         function (e){
@@ -26,12 +27,40 @@ function savelocalStorage(){
                 window.alert("Key, Memo　はいずれも必須です。");
                 return;
             } else {
-                localStorage.setItem(key,value);
+                localStorage.setItem(key, value);
+                viewStorage();              // localStorage からのデータの取得とテーブルへ表示
                 let w_msg = "LocalStorageに" + key + " " + value + "を保存しました。";
                 window.alert(w_msg);
                 document.getElementById("textKey").value = "";
                 document.getElementById("textMemo").value = "";
             }
-        }
+        }, false
     );
+};
+
+//localStorageからのデータの取得とテーブルへ表示
+function viewStorage() {
+
+    const list = document.getElementById("list");
+    //htmlのテーブル初期化
+    while (list.row[0]) list.deleteRow(0);
+
+    //localStorage すべての情報の取得
+    for (let i=0; i<localStorage.length; i++) {
+        let w_key = localStorage.key(i);
+
+        //localStorage のキーと値を表示
+        let tr = document.createElement("tr");
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let td3 = document.createElement("td");
+        list.appendChild(tr);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        td1.innerHTML = "<input name='radio1' type='radio'>" ;
+        td2.innerHTML = w_key;
+        td3.innerHTML = localStorage.getItem(w_key);
+    }
 };

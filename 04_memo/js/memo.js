95fc74956c5b1006637a2aac85879b00e795100a
HaHaoNhien <pandagau2211@gmail.com>
2022-11-08 12:35:18 +0900

    many check

--- 04_memo/js/memo.js
+++ 04_memo/js/memo.js
@@ -45,25 +45,31 @@ function saveLocalStorage(){
     );
 };
 
-        // 3.LocalStorageから１件削除
+        // 3.LocalStorageから選択されている行を削除  //versiob-up3 chg 
         function delLocalStorage() {
             const del = document.getElementById("del");
             del.addEventListener("click",
                 function(e) {
                     e.preventDefault();
-                    let w_sel = "0";            //選択されていれば、”１”が返却される
-                    //w_sel = selectRadioBtn();
-                    w_sel = selectCheckBox();   //テーブルからデータ選択　version-up2 chg: selectRadioBtn ==> selectCheckBox
+                    const chkbox1 = document.getElementsByName("chkbox1");  //version-up3 add
+                    const table1 = document.getElementById("table1");       //version-up3 add
+                    let w_cnt = "0";            //選択されているチェックボックスの数が返却される    //version-up3 w_sel="0" ==> w_cnt = 0
+                    w_cnt = selectCheckBox("del");    //テーブルからデータ選択    //version-up3 chg w_sel ===> w_cnt 引数: なし ==> "del"
 
-                    if(w_sel === "1"){
-                            const key = document.getElementById("textKey").value;
-                            const value = document.getElementById("textMemo").value;
-                        let w_confirm = window.confirm("LocalStorageに\n 「" + key + " " + value +  "」 \n を削除 (delete) しますか？"); //version-up1 add
+                    if(w_cnt >= 1){     //version-up3 chg w_sel === "1" ===> w_cnt >= 1
+                            // const key = document.getElementById("textKey").value;        //version-up3 del
+                            // const value = document.getElementById("textMemo").value;     //version-up3 del
+                        let w_confirm = window.confirm("LocalStorageから選択されている" + w_cnt + "件を削除 (delete) しますか？"); //version-up3 chg
                         //確認ダイアログで「OK」を押されたとき、削除する　version-up1 add
                         if (w_confirm === true) {
+                            for(let i=0; i < chkbox1.length; i++) {        // version-up3 add
+                                if(chkbox1[i].checked) {       // version-up3 add
+                                    localStorage.removeItem(table1.rows[i+1].cells[1].firstChild.data); //version-up3 chg
+                                }   //version-up3 add
+                            }
                             localStorage.removeItem(key);
                             viewStorage();  //localStorageからのデータの取得とテーブルへ表示
-                            let w_msg = "LocalStorageから" + key + "　" + value + "を削除（delete)　しました。";
+                            let w_msg = "LocalStorageから" + i + "件を削除（delete)　しました。";  //version-up3 chg
                             window.alert(w_msg);
                             document.getElementById("textKey").value = "";
                             document.getElementById("textMemo").value = "";
@@ -100,13 +106,13 @@ function saveLocalStorage(){
                 function(e) {
                     e.preventDefault;
                    //w_sel = selectRadioBtn();
-                   selectCheckBox();   //テーブルからデータ選択　version-up2 chg: selectRadioBtn ==> selectCheckBox
+                   selectCheckBox("select");   //テーブルからデータ選択　version-up2 chg: selectRadioBtn ==> selectCheckBox
                 }, false
             );
         };
             //テーブルからデータ選択
-        function selectCheckBox() {     //version-up2 chg: selectRadioBtn ==> selectCheckBox
-            let w_sel = "0";    //選択されていれば、”1”にする
+        function selectCheckBox(mode) {     //version-up3 chg: 引数：なし==>mode
+            //let w_sel = "0";    //選択されていれば、”1”にする //version-up3 del
             let w_cnt = 0;      //選択されているチェックボックスの数 //version-up2 add
             const chkbox1 = document.getElementsByName("chkbox1"); //version-up2 chg: radio1 ==> chkbox1
             const table1 = document.getElementById("table1");
@@ -125,11 +131,22 @@ function saveLocalStorage(){
             }
             document.getElementById("textKey").value = w_textKey;
             document.getElementById("textMemo").value = w_textMemo;
-            if(w_cnt === 1){
-                return w_sel = "1";
-            }else{
-                window.alert("１つ選択（select）してください。");
-            }
+
+            if (selectCheckBox(mode === "select")) { //version-up3 add
+                if(w_cnt === 1){
+                    return w_cnt;   // version-up3 chg w_sel = "1" ==> w_cnt
+                }else{
+                    window.alert("１つ選択（select）してください。");
+                }
+            }   //version-up3 add
+
+            if (selectCheckBox(mode === "del")) { //version-up3 add
+                if(w_cnt >= 1){
+                    return w_cnt;   // version-up3 chg w_sel = "1" ==> w_cnt
+                }else{
+                    window.alert("１つ選択（select）してください。");
+                }
+            }   //version-up3 add
         };
 
 //localStorageからのデータの取得とテーブルへ表示

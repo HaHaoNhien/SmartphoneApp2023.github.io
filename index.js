b30d2f1a9e7bf388ebd66dfde622d7694e35906c
HaHaoNhien <109118319+HaHaoNhien@users.noreply.github.com>
2023-02-07 11:26:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ window.addEventListener(
     item.forEach(function (element, index) {
       setTimeout(function () {
         element.classList.add('fade-in')
-      }, 100 * index)
+      }, 300 * index)
     })
   },
   false,
